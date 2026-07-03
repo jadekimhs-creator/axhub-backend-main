6b64c3c8cb1c38c77c5a324da27a42e76cc5221f
jade <jade.kimhs@gmail.com>
2026-07-03 23:13:34 +0900

    Fix MyBatis startup issue in Spring Boot 4

---
+++ src/main/java/io/shinhanlife/axhub/common/config/MybatisConfig.java
@@ -0,0 +1,19 @@
+package io.shinhanlife.axhub.common.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        return sessionFactory.getObject();
+    }
+}
